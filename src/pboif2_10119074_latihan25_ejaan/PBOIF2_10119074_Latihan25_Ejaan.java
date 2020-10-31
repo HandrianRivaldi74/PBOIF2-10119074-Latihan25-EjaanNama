f20f0824ca18d5b4493a2887d767e53a7447a1fb
HandrianRivaldi74 <handrianr28@gmail.com>
2020-10-31 20:41:50 +0700

    Latihan25

---
+++ src/pboif2_10119074_latihan25_ejaan/PBOIF2_10119074_Latihan25_Ejaan.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119074_latihan25_ejaan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Acer
+ * NAMA     : Handrian Rivaldi
+ * KELAS    : PBOIF2
+ * NIM      : 10119074
+ * Deskirfsi : Program enjaan nama 
+ */
+public class PBOIF2_10119074_Latihan25_Ejaan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan nama depan anda untuk di eja : ");
+        String nama = scanner.next();
+        System.out.println();
+        System.out.printf("Ejaan untuk %s adalah : %n", nama);
+        for (int i = 0; i < nama.length(); i++) {
+            System.out.printf("Huruf ke-%d : %c%n", i+1, nama.charAt(i));
+        }
+    }
+    
+}
